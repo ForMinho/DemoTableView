8ac28b8b0facfccf7c6a990cabaf69955387769b
For_SHINee <for_shinee@For-SHINeedeMacBook-Pro.local>
2014-07-16 09:18:16 +0800

    Initial Commit

---
+++ DemoTableViewTests/DemoTableViewTests.m
@@ -0,0 +1,34 @@
+//
+//  DemoTableViewTests.m
+//  DemoTableViewTests
+//
+//  Created by For_SHINee on 14-7-16.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DemoTableViewTests : XCTestCase
+
+@end
+
+@implementation DemoTableViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
